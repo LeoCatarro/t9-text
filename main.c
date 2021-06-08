6c1351f869ffb72755e52528f67f67309cd034bd
leocatarro <leonardocatarro@gmail.com>
2021-06-08 14:54:17 +0100

    Bug fixed with - or ' in words

--- main.c
+++ main.c
@@ -23,19 +23,25 @@
 wchar_t *CleanWordProcess(wchar_t* word)
 {
     wchar_t *tmpWord = (wchar_t*)malloc(sizeof(wchar_t*)*wcslen(word));
-
     //Clean word process
     for(int i=0 ; i<wcslen(word); i++)
     {
         //Remove '-' and '\''(apostrophe) from the word
-        if(word[i] != '-' || word[i] != '\'')
+        if('-' == word[i] || '\'' == word[i])
+        {
+            //printf("Finded '-' in word\n");
+            break;  
+        }
+        else
         {
             tmpWord[i]= towlower(word[i]);
-        }   
+        }
     }
-    wchar_t *cleanWord = (wchar_t*)malloc(sizeof(wchar_t*)*wcslen(tmpWord));
-    wcscpy(cleanWord, tmpWord);
-    return cleanWord;
+    printf("Word after clean process: %ls\n", tmpWord);
+    wchar_t *cleanWord2 = (wchar_t*)malloc(sizeof(wchar_t*)*wcslen(tmpWord));
+    wcscpy(cleanWord2, tmpWord);
+    free(tmpWord);
+    return cleanWord2;
 }
 
 
@@ -57,14 +63,18 @@ int main()
     InsertT9Keys(KeysTable);
     PrintHashKeysTable(KeysTable);
 
+    wchar_t *tmpWord;
+    wchar_t *cleanWord;
+
     while(fwscanf(fp,L"%ls", buffer) != EOF)
     {
-        wchar_t *tmpWord = (wchar_t*)malloc(sizeof(wchar_t*)*BUFFER_LENGTH);
+        tmpWord = (wchar_t*)malloc(sizeof(wchar_t*)*wcslen(buffer));
         wcscpy(tmpWord, buffer);
         printf("Line Size: %ld\n", wcslen(tmpWord));
         printf("Line: %ls\n", tmpWord);
-        wchar_t *cleanWord = (wchar_t*)malloc(sizeof(wchar_t*)*wcslen(tmpWord));
+        cleanWord = (wchar_t*)malloc(sizeof(wchar_t*)*wcslen(tmpWord));
         cleanWord = CleanWordProcess(tmpWord);
+        cleanWord[wcslen(cleanWord)] = '\0';
         printf("cleanWord Size: %ld\n", wcslen(cleanWord));
         printf("Cleaned Word: %ls\n", cleanWord);
         unsigned int res = StringToIntAccordingT9Keys(cleanWord, KeysTable);
@@ -74,6 +84,8 @@ int main()
     }
 
     PrintHashWordsTable(WordsTable);
+    free(tmpWord);
+    free(cleanWord);
     fclose(fp);
     return 0;
 }
