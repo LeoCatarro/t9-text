65a3ec013a77f6956b5df49a632edfa8dcb5131b
leocatarro <leonardocatarro@gmail.com>
2021-06-10 13:45:35 +0100

    OpenFile && CloseFile function

--- MCurtas.c
+++ MCurtas.c
@@ -2,6 +2,8 @@
 //  Compile line: gcc -std=c99 -Wall hashtable_words.c hashtable_keys.c main.c -o main -lm
 //
 
+// Notes: code for read a file to wchar_t array : https://www.ibm.com/docs/en/i/7.4?topic=lf-fwscanf-read-data-from-stream-using-wide-character
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,7 +21,7 @@
 #define KEYS_TABLE_SIZE 10
 
 //Open the dictionary via fileName passed as argument of the program
-FILE *FileOpen(char *fileName)
+FILE *OpenFile(char *fileName)
 {
     FILE *fp;
     char filePath[BUFFER_LENGTH] = "dictionaries/";
@@ -30,6 +32,15 @@ FILE *FileOpen(char *fileName)
     return fp;
 }
 
+
+//Close the dictionary
+void CloseFile(FILE *file)
+{
+    fclose(file);
+}
+
+
+//Processes the file readed word, converting it to lowercase, and "remove" '-' or '\''
 wchar_t *CleanWordProcess(wchar_t* word)
 {
     wchar_t *tmpWord = (wchar_t*)malloc(sizeof(wchar_t*)*wcslen(word));
@@ -53,6 +64,7 @@ wchar_t *CleanWordProcess(wchar_t* word)
     free(tmpWord);
     return cleanWord2;
 }
+
 
 //Read the lines from the file and insert them into WordsTable, after line been processed and word cleaned!
 void ProcessData(FILE *fp, HashTable KeysTable, HashTable WordsTable)
@@ -79,21 +91,28 @@ void ProcessData(FILE *fp, HashTable KeysTable, HashTable WordsTable)
     printf("Lines loaded successfuly!\n");
 }
 
+
+
+
+
 int main(int argc, char* argv[])
 {   
-    // Notes: code for read a file to wchar_t array : https://www.ibm.com/docs/en/i/7.4?topic=lf-fwscanf-read-data-from-stream-using-wide-character
     setlocale(LC_ALL, "");
+    
+    //Open dictionary
     FILE *fp;
+    fp = OpenFile(argv[1]);
 
-    fp = FileOpen(argv[1]);
-
+    //Initialize table for Keys and Words
     HashTable KeysTable = InitializeKeysTable(KEYS_TABLE_SIZE);
     HashTable WordsTable = InitializeWordsTable(WORDS_TABLE_SIZE);
 
+    //Read the dictionary, process word by word and insert them in WordsTable
     ProcessData(fp, KeysTable, WordsTable);
 
     PrintHashWordsTable(WordsTable);
 
-    fclose(fp);
+    //Close dictionary
+    CloseFile(fp);
     return 0;
 }
